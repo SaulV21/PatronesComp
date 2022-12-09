121ebbb1831ffc52c9d31810c1e35a2dc2dc0195
Saul <claudio.velecela.est@tecazuay.edu.ec>
2022-12-09 09:29:47 -0500

    act

--- src/Controlador/ControllerStrategy.java
+++ src/Controlador/ControllerStrategy.java
@@ -7,12 +7,8 @@
 package Controlador;
 import Modelo.Strategy.Multiplicar;
 import Modelo.Strategy.OperacionAritmetica;
-import Modelo.Strategy.Orden;
-import Modelo.Strategy.PayPal;
-import Modelo.Strategy.PayStrategy;
 import Modelo.Strategy.Restar;
 import Modelo.Strategy.Sumar;
-import Modelo.Strategy.TarjetaCredito;
 import Vista.JFProductos;
 import Vista.VistaMenu;
 import java.io.BufferedReader;
@@ -20,7 +16,6 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import javax.swing.ButtonGroup;
-import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 
 /**
@@ -30,8 +25,7 @@ import javax.swing.JRadioButton;
 public class ControllerStrategy {
     JFProductos vista;
     VistaMenu vistame;
-    JRadioButton r1,r2,r3,r4;
-    ControllerStrategy ce;
+
     
 ButtonGroup g1;
     public ControllerStrategy(JFProductos vista, VistaMenu vistame) {
@@ -42,16 +36,7 @@ ButtonGroup g1;
     }
     
     
-       private static Map<Integer, Integer> priceOnProducts = new HashMap<>();
-    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    private static Orden order = new Orden();
-    private static PayStrategy strategy;
-     static {
-        priceOnProducts.put(1, 2200);
-        priceOnProducts.put(2, 1850);
-        priceOnProducts.put(3, 1100);
-        priceOnProducts.put(4, 890);
-    }
+  
     
     public void iniciarCommand(){
     vista.getBtnContinuar().addActionListener(l->verificar());
@@ -75,96 +60,7 @@ private void verificar(){
         System.out.println("Resta: " + resta );
         System.out.println("Multiplicacion: " + multip );
 }
-      String [] botones={"SI","NO"};
-           String [] pago={"Paypal","TarjetaCredito"};
-            String [] op={"Pagar","Continuar"};
-          int x=0;
-          int count;
-           public void Inicio() {
-              
-               while (!order.isClosed()) {
-                   
-                   int cost;
-                   int choice = 0;
-                   int continueChoice;
-                   do {
-                       
-                       System.out.print("Please, select a product:" + "\n" +
-                               "1 - Mother board" + "\n" +
-                               "2 - CPU" + "\n" +
-                               "3 - HDD" + "\n" +
-                               "4 - Memory" + "\n");
-                       System.out.println("    Seleccione una opcion");
-                       //choice = Integer.parseInt(reader.readLine());
-                       
-                       int  numdigitar=Integer.parseInt(vista.getTxtNumero().getText());
-                       cost = priceOnProducts.get(numdigitar);
-                       System.out.print("Cantidad: ");
-                       //count = Integer.parseInt(reader.readLine());
-                       int  numc=Integer.parseInt(vista.getTxtcantidad().getText());
-                       order.setTotalCost(cost*numc);
-                       
-                        x = JOptionPane.showOptionDialog(null, "¿Desea continuar seleccionando productos?","Opcion",JOptionPane.YES_NO_OPTION,JOptionPane.INFORMATION_MESSAGE,null,botones,botones[0]);
-                       
-                       System.out.print("Desea seguir selecionando? Y/N: ");
-                       
-                       //continueChoice = Integer.parseInt(x.);
-                   } while (x!=1);
-                   
-                   if (strategy == null) {
-                       
-                       x=JOptionPane.showOptionDialog(null, "¿Seleccione metodo de pago?","Opcion",JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE,null,pago,pago[0]);
-                      
-                              
-                       
-                       
-                       // Client creates different strategies based on input from user,
-                       // application configuration, etc.
-                       if (x==0) {
-                         
-                           
-                           String correo =JOptionPane.showInputDialog(null,"Escriba su correo","Dialogo",JOptionPane.QUESTION_MESSAGE);
-                           String contraseña =JOptionPane.showInputDialog(null,"Escriba su contraseña","Dialogo",JOptionPane.QUESTION_MESSAGE);
-                           strategy = new PayPal();
-                           JOptionPane.showMessageDialog(null, " Realizo su transaccion con exito" );
-                       } else {
-                           
-                           String numt =JOptionPane.showInputDialog(null,"Numero de Tarjeta","Dialogo",JOptionPane.QUESTION_MESSAGE);
-                           String expedicion =JOptionPane.showInputDialog(null,"Fecha de Expiracion de Tarjeta","Dialogo",JOptionPane.QUESTION_MESSAGE);
-                           String cvv =JOptionPane.showInputDialog(null,"Escriba CVV","Dialogo",JOptionPane.QUESTION_MESSAGE);
-                           strategy = new TarjetaCredito();
-                           JOptionPane.showMessageDialog(null, " Realizo su transaccion con exito" );
-                           
-                       }
-                       
-                          
-                   }
-                   order.processOrder(strategy);
-                   x=JOptionPane.showOptionDialog(null, "¿Desea pagar ?"+ order.getTotalCost()+"o Continuar","Opcion",JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE,null,op,op[0]);
-                  // System.out.print("Pay " + order.getTotalCost() + " Continuar O pagar? P/C: ");
-                   
-                   if (x==0) {
-                       // Finally, strategy handles the payment.
-                       if (strategy.pay(order.getTotalCost())) {
-                           JOptionPane.showMessageDialog(null, " Realizo su transaccion con exito" );
-                           
-                       } else {
-                           JOptionPane.showMessageDialog(null, " Compruebe los datos" );
-                       }
-                       order.setClosed();
-                       
-                   }
-                   
-                 
-                
-                   
-               }
-        
-    }
-   
     
-
-
     private void cerrar(){
     vista.dispose();
     vistame.setVisible(true);
